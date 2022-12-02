fdaa45ea213bf5b89f16b039dedb0664fcf511d3
travis <trvsmartin@gmail.com>
2022-12-02 07:28:06 -0800

    update hero styling
    <body>
    
    Issue #

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import CallToAction from "../../components/CallToAction/CallToAction";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
-import TopHeader from '../../components/TopHeader/TopHeader'
+import TopHeader from "../../components/TopHeader/TopHeader";
 
 const LandingPage = () => {
-  return <div>
-    <Hero/>
-<Banner content={{text: 'Ready to take the next step?', button: 'Learn More', target: '#'}}/>
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <Banner
+        content={{
+          text: "Ready to take the next step?",
+          button: "Learn More",
+          target: "#",
+        }}
+      />
+      <CallToAction/>
+    </div>
+  );
 };
 
 export default LandingPage;
