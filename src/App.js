4a6ac6657ac8314bdf8a591933e9fd85f1717823
travis <trvsmartin@gmail.com>
2022-12-02 06:25:10 -0800

    add Hero carousel
    <body>
    
    Issue #

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import "./Hero.scss";
 import Banner from "../Banner/Banner";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function Hero() {
   return (
@@ -10,11 +11,47 @@ export default function Hero() {
           <p className="hero__text">we have a new app</p>
           <button className="hero__cta">use it!</button>
         </div>
-        <div className="carousel">
-          <div className="carousel__inner">carousel</div>
+        <Carousel>
+          <Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src={require('../../assets/images/Upload-video-preview.jpg')}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+              />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         </div>
-      </div>
-      <Banner />
-    </div>
+        <Banner />
+        </div>
   );
 }
