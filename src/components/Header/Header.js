dc20025bba933c462278646a0010e2192be70dea
Ian DC <jokerzamba@hotmail.com>
2022-12-01 21:05:29 -0800

    Add basic structure to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <div className="header__logo">LOGO</div>
+        <ul className="header__ul">
+          <Link to="#" className="header__link">
+            Link One
+          </Link>
+          <Link to="#" className="header__link">
+            Link two
+          </Link>
+          <Link to="#" className="header__link">
+            Link three
+          </Link>
+          <Link to="#" className="header__link">
+            Link four
+          </Link>
+          <Link to="#" className="header__link">
+            Link five
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
