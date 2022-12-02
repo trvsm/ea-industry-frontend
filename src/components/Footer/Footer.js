7304f3a6fb016074b471936ff11b4bf65ef5cc42
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-12-02 13:52:30 -0800

    footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,39 +1,44 @@
 import { Link } from "react-router-dom";
 import { AiOutlineDown } from "react-icons/ai";
-import EAlogo from "../../assets/logo/EALogo.png";
+import Flag from "../../assets/icons/Canada-Flag.svg";
+import Ea from "../../assets/images/EA.svg"
 import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__wrap">
-        <div className="footer__logo-div">
-          <div className="footer__logo"></div>
+    <footer className="ffooterr">
+      <div className="ffooterr__wrap">
+        <div className="ffooterr__logo-div">
+          <img className="ffooterr__logo"
+          src={Ea}
+          alt="EA rounded logo in pink">
+
+          </img>
         </div>
 
-        <div className="footer__content">
-          <div className="footer__main">
-            <div className="footer__main-links">
-              <Link className="footer__link">Game Library</Link>
-              <Link className="footer__link">Subscribe</Link>
-              <Link className="footer__link">Redeem Code</Link>
-              <Link className="footer__link">EA App</Link>
-              <Link className="footer__link">About</Link>
-              <Link className="footer__link">Accessibility</Link>
-              <Link className="footer__link">Help</Link>
+        <div className="ffooterr__content">
+          <div className="ffooterr__main">
+            <div className="ffooterr__main-links">
+              <Link className="ffooterr__link">Game Library</Link>
+              <Link className="ffooterr__link">Subscribe</Link>
+              <Link className="ffooterr__link">Redeem Code</Link>
+              <Link className="ffooterr__link">EA App</Link>
+              <Link className="ffooterr__link">About</Link>
+              <Link className="ffooterr__link">Accessibility</Link>
+              <Link className="ffooterr__link">Help</Link>
             </div>
 
-            <div className="footer__main-language">
-              <div className="footer__box">
-                <div className="footer__box-text">Language</div>
-                <div className="footer__box-drop">
+            <div className="ffooterr__main-language">
+              <div className="ffooterr__box">
+                <div className="ffooterr__box-text">Language</div>
+                <div className="ffooterr__box-drop">
                   <img
-                    src={EAlogo}
+                    src={Flag}
                     alt="Country Flag"
-                    className="footer__drop-flag"
+                    className="ffooterr__drop-flag"
                   />
-                  <div className="footer__drop-text">United States</div>
-                  <div className="footer__drop-arrow">
+                  <div className="ffooterr__drop-text">Canada</div>
+                  <div className="ffooterr__drop-arrow">
                     <AiOutlineDown />
                   </div>
                 </div>
@@ -41,27 +46,27 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="footer__links-sec-div">
-            <div className="footer__links-sec">
-              <Link className="footer__link-sec">Legal & Privacy</Link>
-              <Link className="footer__link-sec">User Agreement</Link>
+          <div className="ffooterr__links-sec-div">
+            <div className="ffooterr__links-sec">
+              <Link className="ffooterr__link-sec">Legal & Privacy</Link>
+              <Link className="ffooterr__link-sec">User Agreement</Link>
             </div>
 
-            <div className="footer__links-sec">
-              <Link className="footer__link-sec">
+            <div className="ffooterr__links-sec">
+              <Link className="ffooterr__link-sec">
                 Privacy & Cookie Policy (Your Privacy Rights)
               </Link>
-              <Link className="footer__link-sec">Online Service Updates</Link>
+              <Link className="ffooterr__link-sec">Online Service Updates</Link>
             </div>
 
-            <div className="footer__links-sec">
-              <Link className="footer__link-sec">Security</Link>
-              <Link className="footer__link-sec">YouTube Terms of Service</Link>
-              <Link className="footer__link-sec">Google Privacy Policy</Link>
+            <div className="ffooterr__links-sec">
+              <Link className="ffooterr__link-sec">Security</Link>
+              <Link className="ffooterr__link-sec">YouTube Terms of Service</Link>
+              <Link className="ffooterr__link-sec">Google Privacy Policy</Link>
             </div>
 
-            <div className="footer__copy">
-              <p className="footer__copy-text">© 2022 Electronic Arts Inc.</p>
+            <div className="ffooterr__copy">
+              <p className="ffooterr__copy-text">© 2022 Electronic Arts Inc.</p>
             </div>
           </div>
         </div>
